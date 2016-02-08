e4b9b66f206e03882a4c9942905657d9f5b267f7
John Spence <johnspence@Johns-MacBook-Air.local>
2016-02-08 12:13:06 -0800

    initial push practice files

---
+++ i_m_not_in_the_loop/1-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void print_hello_holberton(void);
+
+int main(void)
+{
+  print_hello_holberton();
+  return (0);
+}
+void print_hello_holberton(void)
+{
+  puts("Hello Holberton");
+}
---
+++ i_m_not_in_the_loop/2-main.c
@@ -0,0 +1,7 @@
+#include "my_functions.h"
+
+int main(void)
+{
+  print_hello_holberton();
+  return(0);
+}
