558ad2c9af86b292c2b675b99b7a5dfef424e481
John Spence <johnjohn987@users.noreply.github.com>
2016-02-18 12:36:03 -0800

    Create my_functions.h

---
+++ keep_calm_and_love_programming/my_functions.h
@@ -0,0 +1,7 @@
+int absolute(int);
+uppercase(char);
+void print_last_digit(int);
+void print_to_98(int);
+void print_combination_2(void);
+void print_number(int n);
+void print_combination_4(void);
