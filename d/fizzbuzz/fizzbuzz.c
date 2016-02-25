f12b31dc8bc0b1153cbad7af1b125493c4ed89d4
John Spence <44@p2-1.hbtn.io>
2016-02-24 16:25:34 -0800

    Reduced number of lines to < 25

--- d/fizzbuzz/fizzbuzz.c
+++ d/fizzbuzz/fizzbuzz.c
@@ -8,24 +8,16 @@ int n;
     if (n == 100){
     	printf ("Buzz");
     } 
-
-  	else if ((n % 3 == 0) && (n % 5 == 0))
-  	{
+  	else if ((n % 3 == 0) && (n % 5 == 0)){
   		printf ("FizzBuzz ");
   	}
-
-    else if (n % 3 == 0)
-    {
+    else if (n % 3 == 0){
        	printf ("Fizz ");
     }
-
-    else if (n % 5 == 0)
-    {
+    else if (n % 5 == 0){
        	printf ("Buzz ");
     }
-
     else printf("%d ", n);
-
    }
 return(0);
 }
