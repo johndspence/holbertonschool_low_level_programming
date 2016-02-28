16dcc2286854522439924013e2d7389e3f027324
John Spence <44@p2-1.hbtn.io>
2016-02-27 17:30:31 -0800

    basic logic drafted

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -1,1 +1,32 @@
-.
+#include <stdio.h>
+
+void triangles_in_the_term(int h, int n){
+
+int total_columns, mid_column, current_row, total_rows, current_triangle
+
+total_columns = (h * 2) - 1;
+mid_column = (h - 1) / 2;
+current_row = 1;
+current_triangle = 1;
+
+/* 1 Triangle loop up to n */
+
+for (; current_triangle , n , current_triangle++)
+
+/* 1.1 Row loop up to h */
+
+/* 1.1.1 Column loop up to total_columns */
+
+loop from 1 to 
+
+int mid_column = h-1
+
+
+printf *
+
+
+printf $
+
+
+
+}
