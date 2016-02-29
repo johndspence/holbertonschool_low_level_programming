006816bf869ec9cf151ef12507645f7f9fd40751
John Spence <44@p2-1.hbtn.io>
2016-02-28 20:01:13 -0800

    Completed 0-triangles.c

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-main.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-main.c
@@ -2,6 +2,6 @@ void triangles_in_the_term(int h, int n);
 
 int main(void)
 {
-  triangles_in_the_term(4, 1);
+  triangles_in_the_term(0, 12);
   return (0);
 }
--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 
 void triangles_in_the_term(int h, int n){
-
-int total_columns, mid_column, current_row, total_rows, current_triangle
-
+int current_column, mid_column, total_columns, current_row, current_triangle;
+mid_column = h;
 total_columns = (h * 2) - 1;
-mid_column = (h - 1) / 2;
-current_row = 1;
-current_triangle = 1;
-
 /* 1 Triangle loop up to n */
-
-for (; current_triangle , n , current_triangle++)
-
-/* 1.1 Row loop up to h */
-
-/* 1.1.1 Column loop up to total_columns */
-
-loop from 1 to 
-
-int mid_column = h-1
-
-
-printf *
-
-
-printf $
-
-
-
-}
+for (current_triangle = 1; current_triangle <= n ; current_triangle++){
+  /* 1.1 Row loop up to h */
+  for (current_row = 1; current_row <= h; current_row++){
+	/* 1.1.1 Column loop up to total_columns */
+	for (current_column = 1;current_column<=total_columns; current_column++){
+	  /* 1.1.1.1 print blanks spaces */
+	  if ((current_column <= (mid_column - current_row))){
+		printf(" ");
+		}
+		/* 1.1.1.2 print *'s */
+		else if ((current_column > (mid_column - current_row)) 
+			&& (current_column < (mid_column + current_row))){
+			printf("*");
+			}
+		}
+	printf("\n");
+	}
+}}
