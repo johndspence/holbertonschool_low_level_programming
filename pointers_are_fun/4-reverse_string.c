ba3dcc3e6c468bb0bc3cc49677b914fbbd3a3802
John Spence <44@p7-2.hbtn.io>
2016-03-07 22:40:46 -0800

    non working copy of 4-reverse_string.c

---
+++ pointers_are_fun/4-reverse_string.c
@@ -0,0 +1,33 @@
+#include "my_functions.h"
+
+/* prints a string using pointer of array str plus 1 */
+void print_string(char *s){
+	
+int i 
+int j
+char temp
+
+	while (*(str+i) != '\0'){
+	i++;
+	}
+
+
+	int i = 0;
+	while ( *(s+i) != '\0' ){
+		print_char( *(str-i));
+		i++;
+}
+
+}
+
+
+
+int str_len(char *str){
+
+	int i = 0;
+
+	while (*(str+i) != '\0'){
+	i++;
+	}
+ return (i);
+}
