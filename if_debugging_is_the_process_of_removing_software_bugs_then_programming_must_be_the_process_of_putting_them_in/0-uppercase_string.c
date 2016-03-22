afc3869dad78c4df4a65086a4492d20bf9a65241
John Spence <44@p7-2.hbtn.io>
2016-03-22 12:01:12 -0700

    Initial copy of 1-cap_string.c

--- if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
+++ if_debugging_is_the_process_of_removing_software_bugs_then_programming_must_be_the_process_of_putting_them_in/0-uppercase_string.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-int i=0;
+// Pass pointer to string array and move each char -32 places on ASCII table
 char *uppercase_string(char *s){
-  for(i=0; (s[i] !='\0'); i++){
+  for(int i=0; (s[i] !='\0'); i++){
      if(s[i]>=97 && s[i]<=122)
       s[i]=s[i]-32;
  }
