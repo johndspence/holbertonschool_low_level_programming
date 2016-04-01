47d907bff9f84e16be93e406bf13ce1b87e4c76c
John Spence <44@p3-5.hbtn.io>
2016-03-31 20:18:56 -0700

    Completed without libraries

---
+++ without_libraries_what_have_we_we_have_no_past_and_no_future/1-cap_string.c
@@ -0,0 +1,33 @@
+int i;
+char *cap_string(char *s){
+  for(i=0; (s[i] !='\0'); i++){
+
+    /* Capitalize First char of the string array */
+    if (s[i] == s[0]){
+      if(s[i]>=97 && s[i]<=122){
+        s[i]=s[i]-32;
+      }
+    }
+
+    /* Capitalize after space */
+    if (s[i] == ' '){
+      if(s[i+1]>=97 && s[i+1]<=122){
+        s[i+1]=s[i+1]-32;
+      }
+    }
+
+    /* Capitalize after tab */
+    if (s[i] == '\t'){
+      if(s[i+1]>=97 && s[i+1]<=122){
+      s[i+1]=s[i+1]-32;
+      }
+    }
+    /* Capitalize after new line */
+    if (s[i] == '\n'){
+      if(s[i+1]>=97 && s[i+1]<=122){
+        s[i+1]=s[i+1]-32;
+      }
+    }
+  }
+return(s);
+}
---
+++ without_libraries_what_have_we_we_have_no_past_and_no_future/1-power.c
@@ -0,0 +1,21 @@
+int power(int x, int y){
+  /* If negative return -1, by requirement */
+  if ((x < 0) || (y < 0)) {
+    return(-1);
+  }
+  /* If negative return -1, by requirement */
+  else if(y == 0) {
+    return(1);
+  }
+  /* A number raised to 1 is that number */
+  else if(y == 1) {
+    return(x);
+  }
+  /* Otherwise multiply the number by itself recursively y times */
+  else {
+    if ((x * power(x, y-1)) < 2147483647) {
+    return(x * power(x, y-1));
+    }
+    }
+return(x);
+}
