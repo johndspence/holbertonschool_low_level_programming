33677c8bdbdb4555ecd85cc5f5e9691763359c4a
John Spence <johndspence@gmail.com>
2016-04-09 00:00:11 -0700

    complete

--- mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/1-advanced_search.c
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/1-advanced_search.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int i = 0;
 
-int advanced_search(int *array, int size, int j){
-  printf("%d\n array[1]=", array[1]);
-  printf("%d\n size=", size);
+int advanced_search(int *array, int size, int (*check_func)(int)){
+  /* printf("%d\n array[1]=", array[1]); */
+  /* printf("%d\n size=", size); */
   while (i < size){
-    printf("%d\n i=", i);
-    printf("%d\n j=", j);
-    if (j == array[i]){
-      return ('1');
+    /* printf("%d\n i=", i);*/
+  /*   printf("%d\n j=", j);*/
+    if ((*check_func)(array[i]) == 1) {
+      return (i);
     }
       i=i+1;
   }
