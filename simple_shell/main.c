8e99ff28165ca611af036f8ffc2bb7ffe18fbbe6
John Spence <johndspence@gmail.com>
2016-05-15 23:54:42 -0700

    Periodic Backup

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -7,11 +7,10 @@
 #include <string.h>
 
 void cmd_prmpt_prntr(void);
-char** cmd_ln_rdr();
+char **cmd_ln_rdr();
 int str_comp(char *, char *);
 void (*blt_in_func_idx(char **cmd_ln_strgs))(char **env);
-void (*path_func_idx(char **cmd_ln_strgs))(char **env);
-int print_char(char c);
+int path_func_idx(char **, char **);
 void (*blt_in_func)(char **);
 
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, char **env)
@@ -21,7 +20,6 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, 
             pid_t pid;
             int status;
             char **cmd_ln_strgs;
-            char *exec_argv[] = {"/usr/bin/ls", "-1", NULL};
 
             /* Command Prompt */
             cmd_prmpt_prntr();
@@ -39,29 +37,34 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv, 
               blt_in_func(env);
             }
             else
-                  {
+                {
                     if ((pid = fork()) == -1)
-                      {
+                        {
                         perror("fork");
                         return 1;
-                      }
+                        }
                     if (pid == 0)
-                      {
+                        {
                         printf("I am the son ! %d\n", pid);
-                        execve(exec_argv[0], exec_argv, env);
-                        /* check the error return value of of execve and continue a cordingly */
-
-                      }
+                        if (path_func_idx(cmd_ln_strgs, env) == 0)
+                            {
+  printf("I have received %d\n", (path_func_idx(cmd_ln_strgs, env)));
+  printf("%s\n", cmd_ln_strgs[0]);
+                              execve ("/bin/ls", cmd_ln_strgs, env);
+                            }
+                        else
+                            {
+                            printf("Unrecognized Command %d\n", pid);
+                            }
+                        }
                     else
-                      {
+                        {
                         printf("I am the father ! %d\n", pid);
                         wait(&status);
                         printf("My son process has terminated with the status:%d\n", status);
-                      }
-                  }
-            /* path_func = path_func_idx(cmd_ln_strngs, env*/
-
-            /* printf("Unrecognized Command %d\n", pid); */
-        }
+                        }
+              /* printf("Unrecognized Command %d\n", pid); */
+            }
+          }
 return 0;
 }
--- simple_shell/path_func_idx.c
+++ simple_shell/path_func_idx.c
@@ -1,10 +1,31 @@
+#include <string.h>
+#include <stdio.h>
 
-void (*path_func_idx(char **cmd_ln_strgs))(char **env)
+
+int path_func_idx(__attribute__((unused)) char **cmd_ln_strgs, char **env)
     {
+      int i;
+      int j;
+char *path;
 
-      if (cmd_ln_strgs[])
+
+      i = 0;
+      j = 0;
 
 
 
+      while (env[i++])
+          {
+          if (env[i][0] == 80 && env[i][1] == 65 && env[i][2] == 84 && env[i][3] == 72)
+              {
+                while (env[i][j])
 
+                    path[j++] = env[i][j++];
+                  }
+              printf("I have froud PATH and will return 0!");
+              printf("%s\n", path);
+              return 0;
+              }
+          }
+      return -1;
     }
