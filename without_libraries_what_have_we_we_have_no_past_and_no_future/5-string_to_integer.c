48631dd0dbbac976d61c1d0c23e91ea2679be18b
John Spence <johndspence@users.noreply.github.com>
2016-06-18 15:54:01 -0700

    Update 5-string_to_integer.c

--- without_libraries_what_have_we_we_have_no_past_and_no_future/5-string_to_integer.c
+++ without_libraries_what_have_we_we_have_no_past_and_no_future/5-string_to_integer.c
@@ -1,1 +1,15 @@
-int string_to_integer(char *s);
+int string_to_integer(char *s){
+    int i=0,sum=0;
+    
+    while(s[i]!='\0'){
+      if(s[i] < 48 || s[i] > 57){
+        printf("Unable to convert it into integer.\n");
+        return 0;
+      }
+      else{
+        sum = sum*10 + (s[i] - 48);
+        i++;
+      }
+    }
+return sum;
+}
