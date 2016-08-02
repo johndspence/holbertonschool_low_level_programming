f2ac04ab583f057444e0d7e8e886b4c18d1b8085
johndspence <johndspence@gmail.com>
2016-08-02 20:11:43 +0000

    Fixed ntree_free.c

--- n_trees/ntree_free.c
+++ n_trees/ntree_free.c
@@ -1,72 +1,31 @@
 #include "tree_hdr.h"
 
-List *free_last_node(List *cur_lst_ptr)
+void free_list_ptr(List *cur_lst_ptr)
 {
-	List *prev_lst_ptr;
-/*if (cur_lst_ptr->node == NULL)
-	return;*/
+	if (cur_lst_ptr == NULL)
+		return;
 
-printf("I started; will iterate until cur_lst_ptr->next == NULL)\n\n");
-	while (cur_lst_ptr->next != NULL)
+	if (cur_lst_ptr->next != NULL)
 	{
-		prev_lst_ptr = cur_lst_ptr;
-		cur_lst_ptr = cur_lst_ptr->next;
-		printf("cur_lst_ptr->next must not have been NULL\n");
+		free_list_ptr (cur_lst_ptr->next);
 	}
-printf("Now I'll check if cur_lst_ptr->node->children == NULL\n\n");
-	if (cur_lst_ptr->node->children == NULL)
-	{
-		printf("cur_lst_ptr->node->children == NULL so I'll free some stuff\n\n");
-		prev_lst_ptr->next = NULL;
-		/*return(cur_lst_ptr);
-		free(cur_lst_ptr->node->children);
-		free(cur_lst_ptr->node->str);
-		free(cur_lst_ptr->node);
-		free(cur_lst_ptr->next);
-		free(cur_lst_ptr);*/
-		return(cur_lst_ptr);
-		printf("So I just freed some stuff\n\n");
 
-	}
-	else
+	if (cur_lst_ptr->node->children != NULL)
 	{
-		cur_lst_ptr = cur_lst_ptr->node->children;
-		printf("cur_lst_ptr->node->children != NULL so I'll recurse\n\n");
-		free_last_node (cur_lst_ptr);
+		free_list_ptr (cur_lst_ptr->node->children);
 	}
-return(cur_lst_ptr);
+	free (cur_lst_ptr->node->str);
+	free (cur_lst_ptr->node);
+	free (cur_lst_ptr);
 }
 
 void ntree_free(NTree *tree)
 {
-	List *root_lst_ptr;
 	List *cur_lst_ptr;
 
 	/*create a list_ptr that points to root node*/
-	root_lst_ptr = new_list();
-	root_lst_ptr->next = NULL;
-	root_lst_ptr->node = tree;
-
-	cur_lst_ptr = free_last_node(root_lst_ptr);
-
-	free(cur_lst_ptr->node->children);
-	free(cur_lst_ptr->node->str);
-	free(cur_lst_ptr->node);
-	free(cur_lst_ptr->next);
-
-	cur_lst_ptr = free_last_node(root_lst_ptr);
-
-	free(cur_lst_ptr->node->children);
-	free(cur_lst_ptr->node->str);
-	free(cur_lst_ptr->node);
-	free(cur_lst_ptr->next);
-
-
-	free (root_lst_ptr->next);
-	free (root_lst_ptr->node->str);
-	free (root_lst_ptr->node->children);
-	free (root_lst_ptr->node);
-	free (root_lst_ptr);
-	/*free (cur_lst_ptr);*/
-
+	cur_lst_ptr = new_list();
+	cur_lst_ptr->next = NULL;
+	cur_lst_ptr->node = tree;
+	free_list_ptr(cur_lst_ptr);
 }
